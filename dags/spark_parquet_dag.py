7247c52ce8c3b8b976e6b4f81d189aaf6c9dd1d7
quimort <quimort97@gmail.com>
2025-08-11 17:03:31 +0200

    add fixes

--- dags/spark_parquet_dag.py
+++ dags/spark_parquet_dag.py
@@ -22,35 +22,16 @@ dag = DAG(
     tags=['spark', 's3', 'parquet', 'etl'],
 )
 
-# Option 1: Using SparkSubmitOperator (recommended)
-convert_csv_to_parquet = SparkSubmitOperator(
-    task_id='convert_csv_to_parquet',
-    application='/app/jobs/spark_parquet_example.py',  # Path inside the container
+# Using SparkSubmitOperator with the configuration you provided, adapted for Python script
+spark_s3 = SparkSubmitOperator(
+    task_id='SparkS3ExampleClient',
+    application='./include/spark_parquet_example.py',  # Path to your Python script
     conn_id='spark_standalone_client',
-    executor_cores=1,
-    executor_memory='1g',
-    driver_memory='1g',
-    dag=dag,
-)
-
-# Option 2: Alternative approach using BashOperator to run docker exec
-# Uncomment this if SparkSubmitOperator doesn't work with your setup
-'''
-convert_csv_to_parquet_bash = BashOperator(
-    task_id='convert_csv_to_parquet_bash',
-    bash_command='docker exec spark-master /home/sparkuser/spark/bin/spark-submit /app/jobs/spark_parquet_example.py',
-    dag=dag,
-)
-'''
-
-# Task to verify the results
-verify_parquet_created = BashOperator(
-    task_id='verify_parquet_created',
-    bash_command='echo "Verification: Check S3 bucket for parquet files at s3a://bucket1/sample_parquet2"',
+    executor_cores=2,
+    total_executor_cores=2,
+    verbose=True,
     dag=dag,
 )
 
 # Set task dependencies
-convert_csv_to_parquet >> verify_parquet_created
-# Uncomment this if using the BashOperator instead
-# convert_csv_to_parquet_bash >> verify_parquet_created
+spark_s3
