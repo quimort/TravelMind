dcb5f9f2d80953845a00e553bc1e779c54bc4142
Joaquim Balletbo <joaquim.balletbo.galan@gmail.com>
2025-08-22 17:06:34 +0200

    10 apis mes fetes

---
+++ landing/hotelOcupacion.py
@@ -0,0 +1,13 @@
+import utils as utils
+
+spark = utils.create_context()
+spark.sparkContext.setLogLevel("ERROR")
+
+path = "https://dataestur.azure-api.net/API-SEGITTUR-v1/EOH_PROV_DL?CCAA=Todos&Lugar%20de%20residencia=Todos&Provincia=Todos"
+db_name = "landing"
+table_name = "hotel_ocupacion"
+df = utils.get_api_endpoint_excel_data(spark, path)
+
+utils.overwrite_iceberg_table(spark, df, db_name, table_name)
+df = utils.read_iceberg_table(spark,db_name,table_name)
+df.show()
---
+++ landing/traficoSemana.py
@@ -0,0 +1,13 @@
+import utils as utils
+
+spark = utils.create_context()
+spark.sparkContext.setLogLevel("ERROR")
+
+path = "https://dataestur.azure-api.net/API-SEGITTUR-v1/INTENSIDAD_TRAFICO_SEMANA_DL?CCAA=Todos&Provincia=Todos"
+db_name = "landing"
+table_name = "trafico_semana"
+df = utils.get_api_endpoint_data(spark, path)
+
+utils.overwrite_iceberg_table(spark, df, db_name, table_name)
+df = utils.read_iceberg_table(spark,db_name,table_name)
+df.show()
