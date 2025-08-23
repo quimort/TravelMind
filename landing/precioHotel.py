fe985e48d89f60c7c8671b91f65b5cbbd74a34c2
Joaquim Balletbo <joaquim.balletbo.galan@gmail.com>
2025-08-23 20:30:30 +0200

    ultimes apis de datestur fetes

---
+++ landing/precioHotel.py
@@ -0,0 +1,13 @@
+import utilsJoaquim as utils
+
+spark = utils.create_context()
+spark.sparkContext.setLogLevel("ERROR")
+
+path = "https://dataestur.azure-api.net/API-SEGITTUR-v1/PRECIOS_ALOJAMIENTOS_HOTELEROS_DL?CCAA=Todos&Provincia=Todos"
+db_name = "landing"
+table_name = "hotel_precio"
+df = utils.get_api_endpoint_data(spark, path)
+
+utils.overwrite_iceberg_table(spark, df, db_name, table_name)
+df = utils.read_iceberg_table(spark,db_name,table_name)
+df.show()
