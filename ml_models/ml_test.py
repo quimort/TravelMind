113b2bb2c0335351c30d0aa520096cdea51e405f
quimort <quimort97@gmail.com>
2025-09-11 16:48:28 +0200

    on cross validation testing

--- ml_models/ml_test.py
+++ ml_models/ml_test.py
@@ -1,10 +1,11 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col
 from pyspark.ml.feature import VectorAssembler
 from pyspark.ml import Pipeline
 import utils as utils
 from pyspark.sql.functions import *
-from pyspark.ml.feature import VectorAssembler
+from pyspark.ml.evaluation import BinaryClassificationEvaluator
+from pyspark.ml.tuning import CrossValidator, ParamGridBuilder
+from pyspark.ml.evaluation import MulticlassClassificationEvaluator
 from xgboost.spark import SparkXGBClassifier
 from pyspark.ml import Pipeline
 from builtins import min as python_min
@@ -194,6 +195,154 @@ if __name__ == "__main__":
         .join(df_apartamentos_features.alias("h"), join_cols, "left")
     )
     print(f"    Joined records: {df_joined.count()}")
-    df_joined.printSchema()
-    df_joined.show(1,truncate=False)
-    # Manejo de
+    
+    # 1. Calcular percentiles 33 y 66
+    quantiles = df_joined.approxQuantile(
+        ["trafico_imd_total", "aire_pct_buena", "apt_availability_score"],
+        [0.33, 0.66],
+        0.01
+    )
+
+    (traf_p33, traf_p66), (aire_p33, aire_p66), (apt_p33, apt_p66) = quantiles
+
+    print("Umbrales tráfico:", traf_p33, traf_p66)
+    print("Umbrales aire:", aire_p33, aire_p66)
+    print("Umbrales alojamiento:", apt_p33, apt_p66)
+
+    # 2. Definir la label binaria
+    df_labeled = df_joined.withColumn(
+        "label",
+        when(
+            (col("trafico_imd_total") > traf_p66) |
+            (col("aire_pct_buena") < aire_p33) |
+            (col("apt_availability_score") < apt_p33),
+            0  # Malo
+        ).otherwise(1)  # Bueno
+    )
+
+    feature_cols = [
+        "apt_viajeros","apt_pernoctaciones","apt_estancia_media","apt_estimados","plazas_estimadas","apt_personal_empleado",
+        "trafico_imd_total","ocio_total_entradas","ocio_gasto_total","ocio_precio_medio_entrada","aire_pct_buena","aire_pct_aceptable",
+        "aire_pct_mala"
+    ]
+    df_labeled = df_labeled.fillna(0, subset=feature_cols)
+    # VectorAssembler para convertir a vector de features
+    assembler = VectorAssembler(
+        inputCols=feature_cols,
+        outputCol="features"
+    )
+    # Contar positivos (label=1) y negativos (label=0)
+    counts = df_labeled.groupBy("label").count().collect()
+
+    # Inicializamos variables
+    num_positivos = 0
+    num_negativos = 0
+
+    for row in counts:
+        if row['label'] == 1:
+            num_positivos = row['count']
+        else:
+            num_negativos = row['count']
+
+    # Ratio para scale_pos_weight
+    ratio_negativos_sobre_positivos = num_negativos / num_positivos
+    # 4. Definir modelo XGBoost
+    xgb = SparkXGBClassifier(
+        features_col="features",       # <- snake_case
+        label_col="label",
+        prediction_col="prediction",
+        probability_col="probability",
+        num_round=50,
+        max_depth=5,
+        eta=0.1,
+        scale_pos_weight=ratio_negativos_sobre_positivos
+    )
+
+    # 5. Construir pipeline
+    pipeline = Pipeline(stages=[assembler, xgb])
+
+    # 6. Entrenar modelo
+    train_df, test_df = df_labeled.randomSplit([0.8, 0.2], seed=42)
+    #model = pipeline.fit(train)
+    #
+    ## 7. Evaluar modelo
+    #predictions = model.transform(test)
+    #predictions.select("año", "mes", "label", "probability", "prediction").show(10, truncate=False)
+    # Evaluador
+    evaluator = MulticlassClassificationEvaluator(
+        labelCol="label",
+        predictionCol="prediction",
+        metricName="accuracy"
+    )
+
+    # Grid de hiperparámetros
+    paramGrid = (ParamGridBuilder()
+        .addGrid(xgb.getParam("max_depth"), [3, 5, 7])
+        .addGrid(xgb.getParam("learning_rate"), [0.05, 0.1, 0.2])
+        .addGrid(xgb.getParam("n_estimators"), [50, 100])  # en lugar de num_boost_round
+        .build()
+    )
+
+    # CrossValidator
+    crossval = CrossValidator(
+        estimator=pipeline,
+        estimatorParamMaps=paramGrid,
+        evaluator=evaluator,
+        numFolds=3,
+        parallelism=2
+    )
+    with mlflow.start_run() as run:
+        # Entrenar
+        model = crossval.fit(train_df)
+        preds = model.transform(test_df)
+
+        # Evaluación
+        evaluator = MulticlassClassificationEvaluator(
+            labelCol="label", predictionCol="prediction", metricName="accuracy"
+        )
+        acc = evaluator.evaluate(preds)
+        # AUC
+        evaluator_auc = BinaryClassificationEvaluator(
+            labelCol="label", rawPredictionCol="prediction", metricName="areaUnderROC"
+        )
+        auc = evaluator_auc.evaluate(preds)
+
+        # F1
+        evaluator_f1 = MulticlassClassificationEvaluator(
+            labelCol="label", predictionCol="prediction", metricName="f1"
+        )
+        f1 = evaluator_f1.evaluate(preds)
+
+        # Precision
+        evaluator_precision = MulticlassClassificationEvaluator(
+            labelCol="label", predictionCol="prediction", metricName="weightedPrecision"
+        )
+        precision = evaluator_precision.evaluate(preds)
+
+        # Recall
+        evaluator_recall = MulticlassClassificationEvaluator(
+            labelCol="label", predictionCol="prediction", metricName="weightedRecall"
+        )
+        recall = evaluator_recall.evaluate(preds)
+
+        print(f"Accuracy: {acc:.3f}")
+        print(f"AUC: {auc:.3f}")
+        print(f"F1: {f1:.3f}")
+        print(f"Precision: {precision:.3f}")
+        print(f"Recall: {recall:.3f}")
+        # Log de parámetros, métricas y modelo
+        mlflow.log_param("max_depth", 5)
+        mlflow.log_param("eta", 0.1)
+        mlflow.log_param("num_round", 50)
+        mlflow.log_param("scale_pos_weight", ratio_negativos_sobre_positivos)
+        mlflow.log_metric("accuracy", acc)
+
+        # Loggear modelo Spark
+        mlflow.spark.log_model(model, "spark_xgb_model")
+
+        print("Modelo registrado en MLflow con run_id:", run.info.run_id)
+    
+    # Supongamos que queremos predecir para Barcelona, 20/09/2025
+    ciudad = "Barcelona"
+    fecha = "2025-09-20"
+    year, month, day = map(int, fecha.split("-"))
